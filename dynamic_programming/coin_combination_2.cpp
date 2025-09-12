adab9a000ecfb3dbb7332535995ca9b0ba01f4b7
Harsh40863 <162051954+Harsh40863@users.noreply.github.com>
2025-09-12 14:18:35 +0530

    Create coin_combination_2.cpp

---
+++ dynamic_programming/coin_combination_2.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+const int mod = 1000000007;
+void printbinary(int num)
+{
+    for (int i = 10; i >= 0; i--)
+    {
+        cout << ((num >> i) & 1);
+    }
+    cout << endl;
+}
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+int msb(int num)
+{
+    int pos = 0;
+    while (num != 0)
+    {
+        pos++;
+        num = num >> 1;
+    }
+    return pos;
+}
+
+int32_t main() {
+        int n,sum;
+        cin>>n>>sum;
+        vector<int>v(n);
+        vector<int>dp(sum+1);
+        for(int i=0;i<n;i++)
+        {
+            cin>>v[i];
+        }
+        dp[0]=1;
+        for(auto it:v)
+        {
+            for(int i=0;i<=sum;i++)
+            {
+                if(i>=it)
+                {
+                    dp[i]=(dp[i]+dp[i-it])%mod;
+                }
+            }
+        }
+        cout<<dp[sum]<<endl;
+
+    
+}
