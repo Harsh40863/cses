98c537f11111395a08b32e301d4e63c892c7dce2
Harsh40863 <162051954+Harsh40863@users.noreply.github.com>
2025-09-13 10:06:50 +0530

    Create removing_digits.cpp

---
+++ dynamic_programming/removing_digits.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+const int mod = 1000000007;
+void printbinary(int num)
+{
+    for (int i = 10; i >= 0; i--)
+    {
+        cout << ((num >> i) & 1);
+    }
+    cout << endl;
+}
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+}
+int msb(int num)
+{
+    int pos = 0;
+    while (num != 0)
+    {
+        pos++;
+        num = num >> 1;
+    }
+    return pos;
+}
+
+int32_t main() {
+   int n;
+   cin>>n;
+   vector<int>dp(n+1,INT_MAX);
+   dp[n]=0;
+   for(int i=n;i>=0;i--)
+   {
+       int x=i;
+       while(x>0)
+       {
+           int num=x%10;
+           dp[i-num]=min(dp[i-num],dp[i]+1);
+           x=x/10;
+       }
+   }
+   cout<<dp[0]<<endl;
+}
